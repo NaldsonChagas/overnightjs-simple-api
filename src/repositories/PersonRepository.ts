dc2b92b41e660ca5bddd7813995281537d837235
Naldson Chagas <naldson.chagas@mobtelecom.com.br>
2021-05-05 18:59:51 -0300

    find one person

--- src/repositories/PersonRepository.ts
+++ src/repositories/PersonRepository.ts
@@ -1,3 +1,11 @@
 import {Repository} from "@src/repositories/Repository"
+import {Person} from "@src/models/Person";
 
-export class PersonRepository extends Repository {}
+export class PersonRepository extends Repository {
+
+  public findById(id: number): Person {
+    const person = this.items.find(person => person.getId() === id)
+    if (!person) throw new Error("This person doesn't exists")
+    return person as Person
+  }
+}
